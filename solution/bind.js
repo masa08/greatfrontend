06bd57a093efd9101181ccfb91d4d24dbf3dc6b9
masa08 <ushimasarug08@gmail.com>
2022-12-12 08:42:47 +0900

    add bind

---
+++ solution/bind.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Object} thisArg
+ * @param {...*} boundArgs
+ * @return {void}
+ */
+Function.prototype.myBind = function (thisArg, ...boundArgs) {
+  const originalFunc = this;
+  if (typeof originalFunc !== 'function') {
+    throw new TypeError('Bind must be called on a function');
+  }
+
+  return function (...args) {
+    return Reflect.apply(originalFunc, thisArg, [...boundArgs, ...args]);
+  };
+};
+
+const john = {
+  age: 42,
+  getAge: function () {
+    return this.age;
+  },
+};
+
+// const unboundGetAge = john.getAge;
+// console.log(unboundGetAge()); // undefined
+
+const boundGetAge = john.getAge.myBind(john);
+console.log(boundGetAge()); // 42
