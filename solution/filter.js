f6ff2b491d36919a07d055c125a1eea108909975
masa08 <ushimasarug08@gmail.com>
2024-11-26 09:03:33 +0900

    updates comment of fliter function

--- solution/filter.js
+++ solution/filter.js
@@ -9,6 +9,7 @@ Array.prototype.myFilter = function (callbackFn, thisArg) {
 
   for (let k = 0; k < len; k++) {
     const kValue = this[k];
+    // 配列の要素が存在しない場合は、Object.hasOwn(this, k) が false になる
     if (Object.hasOwn(this, k) && callbackFn.call(thisArg, kValue, k, this)) {
       results.push(kValue);
     }
