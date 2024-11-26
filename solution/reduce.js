7f947178e0100dfb3693dba1690d8a94c7affab3
masa08 <ushimasarug08@gmail.com>
2024-11-26 12:17:35 +0900

    updates name of reduce

--- solution/reduce.js
+++ solution/reduce.js
@@ -6,19 +6,20 @@
 Array.prototype.myReduce = function (callbackFn, initialValue) {
   const noInitialValue = initialValue === undefined;
   const len = this.length;
-  if (noInitialValue && len === 0)
+  if (noInitialValue && len === 0) {
     throw new TypeError('Reduce of empty array with no initial value');
+  }
 
-  let acc = noInitialValue ? this[0] : initialValue;
+  let accumulater = noInitialValue ? this[0] : initialValue;
   let startingIndex = noInitialValue ? 1 : 0;
 
   for (let k = startingIndex; k < len; k++) {
     if (Object.hasOwn(this, k)) {
-      acc = callbackFn(acc, this[k], k, this);
+      accumulater = callbackFn(accumulater, this[k], k, this);
     }
   }
 
-  return acc;
+  return accumulater;
 };
 
 console.log([1, 2, 3].myReduce((prev, curr) => prev + curr, 4));
