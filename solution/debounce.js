c4f41fa58daac8b3630c961c68c552b4f102388f
masa08 <ushimasarug08@gmail.com>
2024-11-29 08:53:51 +0900

    updates func of debounce

--- solution/debounce.js
+++ solution/debounce.js
@@ -2,7 +2,10 @@ export default function debounce(func, wait = 0) {
   let timeoutId = null;
 
   return function (...args) {
+    // この関数が呼び出されたときに、thisがどのオブジェクトを指すかを保持するため
     const context = this;
+
+    // すでにタイマーがセットされている場合は、それをクリアする
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
       timeoutId = null;
